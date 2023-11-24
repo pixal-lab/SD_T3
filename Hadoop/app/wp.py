30a41ce72a1bada30484db0bf33a35289a68e15b
pixal <pixal@pix-notebook.pixnet.com>
2023-11-24 19:12:36 -0300

    .

--- Hadoop/app/wp.py
+++ Hadoop/app/wp.py
@@ -11,7 +11,32 @@ entradas = [
     "ferrari", 
     "lamborghini",
     "lotus", 
-    "bugatti"
+    "bugatti",
+
+    "Jaws_(film)",
+    "Star_Wars_(film)",
+    "The_Matrix",
+    "Pulp_Fiction",
+    "The_Godfather",
+
+    "Titanic_(1997_film)",
+    "The_Shawshank_Redemption",
+    "Jurassic_Park",
+    "The_Dark_Knight",
+    "Inception",
+
+    "The_Legend_of_Zelda:_Ocarina_of_Time",
+    "Super_Mario_Bros",
+    "Minecraft",
+    "Grand_Theft_Auto_V",
+    "The_Elder_Scrolls_V",
+
+    "World_of_Warcraft",
+    "Fortnite",
+    "Pokemon",
+    "League_of_Legends",
+    "Dota_2",
+
 ]
 url = "https://en.wikipedia.org/w/api.php"
 i = 1
@@ -38,12 +63,20 @@ for entrada in entradas:
     url_document = req['query']['pages'][n_page]['fullurl']
 
     if i <= 15:
-        with open(f'../carpeta1/{entrada}.txt', 'w') as f:
-            f.write(texto)
+        try:
+            with open(f'carpeta1/{entrada}.txt', 'w') as f:
+                f.write(texto)
+        except:
+            with open(f'carpeta1/{entrada}.txt', 'a') as f:
+                f.write(texto)
     else:
-        with open(f'../carpeta2/{entrada}.txt', 'w') as f:
-            f.write(texto)
-    with open('../data.txt', 'a') as f:
+        try:
+            with open(f'carpeta2/{entrada}.txt', 'w') as f:
+                f.write(texto)
+        except:
+            with open(f'carpeta2/{entrada}.txt', 'a') as f:
+                f.write(texto)
+    with open('data.txt', 'a') as f:
         f.write(f'{i}\t{url_document}\n')
     i = i + 1
 
---
+++ README.md
@@ -0,0 +1,13 @@
+# Implementacion Hadoop
+## Uso
+
+Creacion y levantamiento de Hadoop
+```sh
+git clone https://github.com/pixal-lab/SD_T3
+cd SD_T3
+docker compose up -d
+```
+
+
+
+
